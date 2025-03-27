33711e8e05bfb27bcb3bf183e93211480f137f05
TGOSS1984 <tomgoss84@gmail.com>
2025-03-27 20:10:37 +0000

    add js to fix hamburger nav menu dropdown

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.querySelector('nav ul');
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
